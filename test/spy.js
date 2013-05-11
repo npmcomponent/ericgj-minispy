64c350a786bc917cd63bf0e0a30cec88eddaa191
Eric Gjertsen <ericgj72@gmail.com>
2013-05-11 17:32:18 -0400

    add tests: call

--- test/spy.js
+++ test/spy.js
@@ -122,12 +122,125 @@ function spyAlwaysCalledTests(method) {
     };
 }
 
+function spyNeverCalledTests(method) {  /* todo */ }
+
+
 
 
 module.exports = {
     
     "calledWith": spyCalledTests("calledWith"),
-    "alwaysCalledWith": spyAlwaysCalledTests("alwaysCalledWith")
+    "alwaysCalledWith": spyAlwaysCalledTests("alwaysCalledWith"),
+    
+    "call": {
+        "calls underlying function": function () {
+            var called = false;
+
+            var spy = Spy(function () {
+                called = true;
+            });
+
+            spy.watch();
+
+            assert(called);
+        },
+
+        "passs arguments to function": function () {
+            var actualArgs;
+
+            var func = function (a, b, c, d) {
+                actualArgs = [a, b, c, d];
+            };
+
+            var args = [1, {}, [], ""];
+            var spy = Spy(func);
+            spy.watch(args[0], args[1], args[2], args[3]);
+
+            assert.deepEqual(actualArgs, args);
+        },
+
+      /* not tested
+        "maintains this binding": function () {
+            var actualThis;
+
+            var func = function () {
+                actualThis = this;
+            };
+
+            var object = {};
+            var spy = Spy(func);
+            spy.watch.call(object);
+
+            assert.deepEqual(actualThis, object);
+        },
+      */
+      
+        "returns function's return value": function () {
+            var object = {};
+
+            var func = function () {
+                return object;
+            };
+
+            var spy = Spy(func);
+            var actualReturn = spy.watch();
+
+            assert.deepEqual(actualReturn, object);
+        },
+
+        "throws if function throws": function () {
+            var err = new Error();
+            var spy = Spy(function () {
+                throw err;
+            });
+
+            try {
+                spy.watch();
+                fail("Expected spy to throw exception");
+            } catch (e) {
+                assert.deepEqual(e, err);
+            }
+        }
+
+      /* Not tested
+        "retains function length 0": function () {
+            var spy = sinon.spy.create(function () {});
+
+            assert.equals(spy.length, 0);
+        },
+
+        "retains function length 1": function () {
+            var spy = sinon.spy.create(function (a) {});
+
+            assert.equals(spy.length, 1);
+        },
+
+        "retains function length 2": function () {
+            var spy = sinon.spy.create(function (a, b) {});
+
+            assert.equals(spy.length, 2);
+        },
+
+        "retains function length 3": function () {
+            var spy = sinon.spy.create(function (a, b, c) {});
+
+            assert.equals(spy.length, 3);
+        },
+
+        "retains function length 4": function () {
+            var spy = sinon.spy.create(function (a, b, c, d) {});
+
+            assert.equals(spy.length, 4);
+        },
+
+        "retains function length 12": function () {
+            var spy = sinon.spy.create(function (a, b, c, d, e, f, g, h, i, j,k,l) {});
+
+            assert.equals(spy.length, 12);
+        }
+      */
+      
+    },    
     
 }
 
